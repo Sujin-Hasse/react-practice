1a228925771ceeb2972752db50a5d6f47617d79e
Sujin-Hasse <2017104858@khu.ac.kr>
2023-05-07 18:52:30 +0900

    chore: use state를 활용해 모달띄우는 기능을 넣기 위한 코드  재구조화

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,11 +1,195 @@
 import Modal from "../components/Modal";
+import styled from "styled-components";
+import logo from "../assets/image/icon_logo.png";
+import kakaoLogin from "../assets/image/icon_kakao.png";
+import googleLogin from "../assets/image/icon_google.png";
+import twitterLogin from "../assets/image/icon_twitter.png";
+import lineLogin from "../assets/image/icon_line.png";
 
 const Main = () => {
   return (
     <>
-      <Modal />
+      {/* 여기는 수진이가 만든 모달 styled-components */}
+      <Background>
+        <WrapModal>
+          <WrapLogo>
+            <Logo src={logo} />
+          </WrapLogo>
+          <LoginText>로그인</LoginText>
+
+          <WrapInput>
+            <IdInput placeholder="이메일" />
+            <PasswordInput placeholder="비밀번호" />
+          </WrapInput>
+
+          <LoginButton>로그인</LoginButton>
+          <CheckPassword>비밀번호를 잊어버리셨나요?</CheckPassword>
+          <MakeAccount>
+            계정이 없으신가요? <MakeAccountButton>회원가입</MakeAccountButton>
+          </MakeAccount>
+          <WrapLine>
+            <Line />
+            <LineText>OR</LineText>
+            <BackLine />
+          </WrapLine>
+
+          <SocialLoginList>
+            <KakaoLogin src={kakaoLogin} />
+            <GoogleLogin src={googleLogin} />
+            <TwitterLogin src={twitterLogin} />
+            <LineLogin src={lineLogin} />
+          </SocialLoginList>
+
+          <LoginTip>
+            TIP.왓챠 계정이 있으신가요? 왓챠와 왓챠피디아는 같은 계정을
+            사용해요.
+          </LoginTip>
+        </WrapModal>
+      </Background>
     </>
   );
 };
+
+const Background = styled.div`
+  display: flex;
+`;
+
+const WrapModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 375px;
+  height: 634px;
+  font-family: inherit;
+  //background-color: blue;
+`;
+
+const WrapLogo = styled.div`
+  text-align: center;
+`;
+
+const Logo = styled.img`
+  justify-content: center;
+  width: 198px;
+  height: 38.02px;
+`;
+
+const LoginText = styled.h2`
+  font-size: 17px;
+  letter-spacing: -0.5px;
+  line-height: 22px;
+  font-weight: 700;
+  text-align: center;
+  margin: 24px 0px 20px;
+`;
+
+const WrapInput = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const IdInput = styled.input`
+  align-items: center;
+  background: rgb(245, 245, 245);
+  box-sizing: border-box;
+  border: none;
+  height: 44px;
+  margin: 4px 0px 4px 0px;
+  padding: 0px 12px 0px 12px;
+  border-radius: 6px;
+`;
+
+const PasswordInput = styled.input`
+  align-items: center;
+  background: rgb(245, 245, 245);
+  box-sizing: border-box;
+  border: none;
+  height: 44px;
+  margin: 4px 0px 4px 0px;
+  padding: 0px 12px 0px 12px;
+  border-radius: 6px;
+`;
+
+const LoginButton = styled.button`
+  background-color: #ff2f6e;
+  border: none;
+  border-radius: 6px;
+  height: 44px;
+  color: white;
+  font-size: 17px;
+  letter-spacing: -0.5px;
+  line-height: 22px;
+  font-weight: 700;
+  text-align: center;
+  margin: 20px 0px 20px 0px;
+`;
+
+const CheckPassword = styled.button`
+  background: none;
+  border: none;
+  color: #ff2f6e;
+  font-size: 15px;
+  font-weight: 400;
+  margin-bottom: 14px;
+`;
+
+const MakeAccount = styled.div`
+  text-align: center;
+  color: #8c8c8c;
+  font-size: 15px;
+  font-weight: 400;
+`;
+
+const MakeAccountButton = styled.button`
+  background: none;
+  border: none;
+  color: #ff2f6e;
+  font-size: 15px;
+  font-weight: 400;
+  margin-bottom: 14px;
+`;
+
+const WrapLine = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 24px 0px 24px 0px;
+`;
+
+const Line = styled.div`
+  border: 0.5px solid gray;
+  width: 200px;
+  height: 1px;
+`;
+
+const LineText = styled.div`
+  margin: 0px 5px 0px 5px;
+  color: gray;
+`;
+
+const BackLine = styled.div`
+  border: 0.5px solid gray;
+  width: 200px;
+  height: 1px;
+`;
+
+const SocialLoginList = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const KakaoLogin = styled.img``;
+const GoogleLogin = styled.img``;
+const TwitterLogin = styled.img``;
+const LineLogin = styled.img``;
+
+const LoginTip = styled.div`
+  margin-top: 48px;
+  background-color: #f7f7f7;
+  color: #8c8c8c;
+  font-size: 15px;
+  font-weight: 400;
+  padding: 10px 13px 10px 13px;
+`;
 
 export default Main;
