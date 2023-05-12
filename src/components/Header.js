1951d82d7a0395a580d583c10a5a0bae74a82659
Sujin-Hasse <2017104858@khu.ac.kr>
2023-05-12 16:20:07 +0900

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,68 +1,7 @@
-import styled from "styled-components";
-import GlobalStyle from "../GlobalStyle";
-import logo from "../assets/image/icon_logo.png";
+import React from "react";
 
 const Header = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <WrapCategory>
-        <Logo src={logo} />
-        <CategoryMovie>영화</CategoryMovie>
-        <CategoryTv>TV</CategoryTv>
-        <CategoryBook>책</CategoryBook>
-        <CategoryWebtoon>웹툰</CategoryWebtoon>
-      </WrapCategory>
-      <HeaderBlock />
-    </>
-  );
+  return <div></div>;
 };
-
-const WrapCategory = styled.div`
-  display: flex;
-`;
-
-const Logo = styled.img`
-  width: 151px;
-  height: 29px;
-  margin: 0px 0px 0px 0px;
-`;
-
-const CategoryMovie = styled.button`
-  border: none;
-  background: none;
-  font-size: 15px;
-  font-weight: 500;
-  margin: 0px 0px 0px 24px;
-  color: #727272;
-`;
-const CategoryTv = styled.button`
-  border: none;
-  background: none;
-  font-size: 15px;
-  font-weight: 500;
-  margin: 0px 0px 0px 24px;
-  color: #727272;
-`;
-const CategoryBook = styled.button`
-  border: none;
-  background: none;
-  font-size: 15px;
-  font-weight: 500;
-  margin: 0px 0px 0px 24px;
-  color: #727272;
-`;
-const CategoryWebtoon = styled.button`
-  border: none;
-  background: none;
-  font-size: 15px;
-  margin: 0px 0px 0px 24px;
-  color: #727272;
-`;
-
-const HeaderBlock = styled.div`
-  margin: 0px 0px 0px 50px;
-  width: 400px;
-`;
 
 export default Header;
