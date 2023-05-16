1d2293a61c1dad4c6874b4bab9b7cc4b2d21e9b1
Sujin-Hasse <2017104858@khu.ac.kr>
2023-05-16 14:40:09 +0900

    git 커밋 되돌리기 완료

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import GlobalStyle from "./GlobalStyle";
+import GlobalStyle from "../src/Globalstyle";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
